1318daba6a524d662b3baf25dbe256d7c7a65666
LukeBeaumont <lbeaumont50@msn.com>
2023-09-20 20:13:47 +0100

    Create initial click functions and input winning combos

--- script.js
+++ script.js
@@ -1,34 +1,45 @@
-let xTurn = true;
-
-const boxes = document.querySelectorAll(".box");
+const winningCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+const x_class = "x";
+const o_class = "o";
+const boxes = document.querySelectorAll("[data-cell]");
+const gameBoard = document.querySelector(".game-board");
+let circleTurn;
 
 boxes.forEach((box) => {
-  box.addEventListener("click", function (e) {
-    handleClick(e),
-      {
-        once: true,
-      };
-    swapPlayer();
-  });
+  box.addEventListener("click", handleClick, { once: true });
 });
 
 function handleClick(e) {
-  if (xTurn == true) {
-    e.target.classList.add("x");
-  } else {
-    e.target.classList.add("o");
-  }
+  const box = e.target;
+  const currentClass = circleTurn ? o_class : x_class;
+  placeMark(box, currentClass);
+  swapTurns();
+  setHoverClass();
 }
 
-function swapPlayer() {
-  const gameBoard = document.querySelector(".game-board");
-  if (xTurn == true) {
-    gameBoard.classList.remove("x");
+function placeMark(box, currentClass) {
+  box.classList.add(currentClass);
+}
+
+function swapTurns() {
+  circleTurn = !circleTurn;
+}
+
+function setHoverClass() {
+  gameBoard.classList.remove("x");
+  gameBoard.classList.remove("o");
+  if (circleTurn) {
     gameBoard.classList.add("o");
-    xTurn = false;
   } else {
-    gameBoard.classList.remove("o");
     gameBoard.classList.add("x");
-    xTurn = true;
   }
 }
